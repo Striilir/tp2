29c2a50d0711d8b278a63429fe924936e04cc8ec
Striilir <Striilir@gmail.com>
2020-05-11 15:32:49 +0200

    HelloLabel initialisé avec le bon nom.

--- src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
+++ src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
@@ -6,6 +6,6 @@ import javafx.stage.Stage;
 public class HelloLabel extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Hello !");
     }
 }

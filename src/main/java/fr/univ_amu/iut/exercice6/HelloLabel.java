24f16d8988cbf89ad0023feba31c8bdf2b7fc5db
Striilir <Striilir@gmail.com>
2020-05-11 16:19:17 +0200

    HelloLabel ajout du label.

--- src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
+++ src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
@@ -1,6 +1,9 @@
 package fr.univ_amu.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloLabel extends Application {
@@ -9,5 +12,13 @@ public class HelloLabel extends Application {
         primaryStage.setTitle("Hello !");
         primaryStage.setHeight(100);
         primaryStage.setWidth(250);
+        BorderPane root = new BorderPane();
+        Scene s = new Scene(root, 100, 250);
+        primaryStage.setScene(s);
+        Label label = new Label("Hello !");
+        label.setId("labelHello");
+        root.setCenter(label);
+        primaryStage.show();
+
     }
 }

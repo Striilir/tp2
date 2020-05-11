d5314569b3ee8d8280e2a410000d3bd2f3337863
Striilir <Striilir@gmail.com>
2020-05-11 15:15:27 +0200

    Implémentation du titre de MySecondJavaFXWindow

--- src/main/java/fr/univ_amu/iut/exercice4/MySecondJavaFXWindow.java
+++ src/main/java/fr/univ_amu/iut/exercice4/MySecondJavaFXWindow.java
@@ -11,6 +11,6 @@ public class MySecondJavaFXWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Second Useless JavaFX Window");
     }
 }
--- src/test/java/fr/univ_amu/iut/exercice4/TestMySecondJavaFXWindow.java
+++ src/test/java/fr/univ_amu/iut/exercice4/TestMySecondJavaFXWindow.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@Disabled
+
 @ExtendWith(ApplicationExtension.class)
 public class TestMySecondJavaFXWindow {
 

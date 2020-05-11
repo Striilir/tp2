654913ca0a327e7dab1d72a43d0a0e53dd30b276
Striilir <Striilir@gmail.com>
2020-05-11 16:40:02 +0200

    HelloBeautifulButton ajouté.

--- src/main/java/fr/univ_amu/iut/exercice8/HelloBeautifulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice8/HelloBeautifulButton.java
@@ -1,11 +1,29 @@
 package fr.univ_amu.iut.exercice8;
 
 import javafx.application.Application;
+import javafx.scene.ImageCursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloBeautifulButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Hello !");
+        primaryStage.setHeight(250);
+        primaryStage.setWidth(250);
+        BorderPane root = new BorderPane();
+        Scene s = new Scene(root, 250, 250);
+        primaryStage.setScene(s);
+        ImageView Img = new ImageView("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        Img.setFitHeight(150);
+        Img.setFitWidth(150);
+        Button button = new Button("Hello !",Img);
+        button.setId("buttonHello");
+        root.setCenter(button);
+        primaryStage.show();
     }
 }
--- src/test/java/fr/univ_amu/iut/exercice8/TestHelloBeautifulButton.java
+++ src/test/java/fr/univ_amu/iut/exercice8/TestHelloBeautifulButton.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.util.NodeQueryUtils.hasText;
 
-@Disabled
 @ExtendWith(ApplicationExtension.class)
 public class TestHelloBeautifulButton {
 
@@ -57,37 +56,32 @@ public class TestHelloBeautifulButton {
         robot.release(new MouseButton[]{});
     }
 
-    @Disabled
     @Test
     public void should_initialize_stage_with_correct_title() {
         assertEquals("Hello !", stage.getTitle());
     }
 
-    @Disabled
     @Test
     public void should_initialize_stage_with_height_of_250() {
         assertEquals(250, stage.getHeight());
     }
 
-    @Disabled
     @Test
     public void should_initialize_stage_with_width_of_250() {
         assertEquals(250, stage.getWidth());
     }
 
-    @Disabled
     @Test
     public void should_initialize_button_with_text_hello() {
         verifyThat("#buttonHello", hasText("Hello !"));
     }
 
-    @Disabled
     @Test
     public void should_initialize_stage_with_useless_button() {
         verifyThat("#buttonHello", (Button node) -> node.getOnAction() == null);
     }
 
-    @Disabled
+
     @Test
     public void should_initialize_stage_with_a_graphic() {
         verifyThat("#buttonHello", (Button node) -> node.getGraphic() instanceof ImageView);
@@ -96,7 +90,6 @@ public class TestHelloBeautifulButton {
         verifyThat("#buttonHello", (Button node) -> ((ImageView) node.getGraphic()).getImage().getWidth() == 150);
     }
 
-    @Disabled
     @Test
     public void should_initialize_stage_is_showing() {
         assertTrue(stage.isShowing());
